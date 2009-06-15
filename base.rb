8cbb9c9c4677b2f2d2aa4fbaebb6e4dfe9fb3f0e
Iain Wood <iain@Soulflyer.local>
2009-06-15 12:21:54 +0700

    message

--- base.rb
+++ base.rb
@@ -7,7 +7,7 @@ config.gem "webrat"
 config.gem "cucumber"
 # config.gem "notahat-machinist", :lib => "machinist"
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com/"
-rake "gems:install"
 END
+rake "gems:install"
 run "cat tmp/test.rb >> config/environments/test.rb"
 run "rm tmp/test.rb"
