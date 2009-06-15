9a421086c6e0e650146aacb22925259f112dfb18
Iain Wood <iain@Soulflyer.local>
2009-06-15 12:01:28 +0700

    message

--- base.rb
+++ base.rb
@@ -1,5 +1,11 @@
 run "echo TODO > README"
 generate :app_layout
-gem "rspec", :lib => false
-gem "rspec-rails", :lib => false
-rake "gems"
+file "config/environments/test.rb", <<-END
+config.gem "rspec", :lib => false
+config.gem "rspec-rails", :lib => false
+config.gem "webrat"
+config.gem "cucumber"
+# config.gem "notahat-machinist", :lib => "machinist"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com/"
+rake "gems:install"
+END
