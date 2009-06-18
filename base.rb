93f36e23f081132cbe8b65919e779082fbc65e78
Iain Wood <iain@Soulflyer.local>
2009-06-18 14:15:36 +0700

    added sudo to rake gems:install

--- base.rb
+++ base.rb
@@ -19,7 +19,7 @@ gem 'thoughtbot-clearance',
   :lib     => 'clearance', 
   :source  => 'http://gems.github.com'
   
-rake "gems:install"
+rake "gems:install", :sudo => true
 rake "gems:unpack"
 generate :clearance
 
