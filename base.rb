47a243ede445492587a7ec173705f3c2457ff645
Iain Wood <public@soulflyer.co.uk>
2009-06-21 15:30:04 +0700

    Put factory girl before cucumber so it appears in cucumber env

--- base.rb
+++ base.rb
@@ -18,8 +18,8 @@ file "/tmp/test.rb", <<-END
 config.gem "rspec", :lib => false
 config.gem "rspec-rails", :lib => false
 config.gem "webrat"
-config.gem "cucumber"
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com/"
+config.gem "cucumber"
 END
 run "cat tmp/test.rb >> config/environments/test.rb"
 run "rm tmp/test.rb"
