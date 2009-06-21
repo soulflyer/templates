8dd6fa586d5117f46e2c33820cc350a02d8d5209
Iain Wood <public@soulflyer.co.uk>
2009-06-21 13:00:58 +0700

    added paperclip

--- base.rb
+++ base.rb
@@ -30,7 +30,9 @@ generate :cucumber
 # = Generate static pages stuff =
 # ===============================
 generate :rspec_scaffold, "page name:string permalink:string content:text"
-gem 'RedCloth', :source => "http://code.whytheluckystiff.net"
+# RedCloth uses native code. It's already installed, so why recompile into vendor every time? Don't forget to
+# check when installing on production server.......
+# gem 'RedCloth', :source => "http://code.whytheluckystiff.net"
 run "rm app/views/layouts/pages.html.erb"
 
 
@@ -44,6 +46,8 @@ gem 'thoughtbot-clearance',
 # rake "gems:install", :sudo => true
 rake "gems:unpack"
 generate :clearance
+# remove the paths file or generate :clearance_features will hang (waiting for confirmation of overwrite)
+run "rm features/support/paths.rb"
 generate :clearance_features
 
 # Add the host details to the config files for clearance
@@ -60,6 +64,11 @@ run "echo DO_NOT_REPLY=\\\"donotreply@example.com\\\" >> config/environment.rb"
 
 # more clearance setup
 route "map.root :controller => 'pages', :id => '1'"
+
+# ==================================
+# = Add paperclip for photo upload =
+# ==================================
+plugin "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 
 # =============
 # = Finish up =
